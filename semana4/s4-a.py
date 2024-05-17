8c8926fd39765ae13fcd6eb65402c36115e4abb8
distobs <cuppotatocake@gmail.com>
2024-05-17 13:01:36 -0300

    s4-a era na verdade s4-b, conserta e adiciona o s4-a real

--- semana4/s4-a.py
+++ semana4/s4-a.py
@@ -1,2 +1,12 @@
-res = sum(list(map(int, input().split(' ')))[:4]) - 3
-print(res)
+line = list(map(int, input().split(' ')))
+n = line[0]
+s = line[1]
+menor = s
+
+for i in range(n):
+    mov = int(input())
+    s += mov
+    if menor > s:
+        menor = s
+
+print(menor)
