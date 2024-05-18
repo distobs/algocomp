782395a78536ff7f774a1b35b9b48ca8cce4c314
distobs <cuppotatocake@gmail.com>
2024-05-18 20:26:13 -0300

    semana 5 c

---
+++ semana5/s5-c.py
@@ -0,0 +1,8 @@
+comprimento1 = int(input())
+largura1 = int(input())
+comprimento2 = int(input())
+largura2 = int(input())
+dim1 = comprimento1 * largura1
+dim2 = comprimento2 * largura2
+
+print(f'{dim1 if dim1 > dim2 else dim2}')
