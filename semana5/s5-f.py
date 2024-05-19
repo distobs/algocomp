d2ed519cc3d905649f99058dd092c789d84d1463
distobs <cuppotatocake@gmail.com>
2024-05-19 16:55:58 -0300

    colinha marota, semana 5 f

--- semana5/s5-f.py
+++ semana5/s5-f.py
@@ -2,7 +2,18 @@ entrada = list(map(int, input().split(' ')))[:4]
 ila, ilb = entrada[0], entrada[1]
 fla, flb = entrada[2], entrada[3]
 
-if ila == fla:
+'''
+colei do github alheio:
+    alternativamente, de forma mais simples:
+        se ila != fla, só precisamos de 1 aperto do c1 caso ilb == flb, ou de 1
+        aperto do c2 caso contrário.
+        se ila == fla, temos os casos:
+            (ib == fb):
+                0
+            (ib != fb):
+                2
+'''
+if ila != fla:
     if ilb == flb:
         print('0')
     elif ilb != flb:
@@ -12,5 +23,5 @@ elif ilb == flb:
         print('0')
     elif ila != fla:
         print('1')
-elif ila != fla or ilb != flb:#1 0 0 1
+elif ila != fla or ilb != flb:
     print('1')
