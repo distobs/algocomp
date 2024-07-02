a5d12560b7fbefa94d5a6b5df3a02272b5b76ed9
distobs <cuppotatocake@gmail.com>
2024-07-01 21:05:48 -0300

    solucao elegante

---
+++ semana10/s10-a.c
@@ -0,0 +1,69 @@
+int fib(int n) {
+    switch (n) {
+    case 0:
+        return 0 ;break;
+    case 1:
+        return 1 ;break;
+    case 2:
+        return 1 ;break;
+    case 3:
+        return 2 ;break;
+    case 4:
+        return 3 ;break;
+    case 5:
+        return 5 ;break;
+    case 6:
+        return 8 ;break;
+    case 7:
+        return 13 ;break;
+    case 8:
+        return 21 ;break;
+    case 9:
+        return 34 ;break;
+    case 10:
+        return 55 ;break;
+    case 11:
+        return 89 ;break;
+    case 12:
+        return 144 ;break;
+    case 13:
+        return 233 ;break;
+    case 14:
+        return 377 ;break;
+    case 15:
+        return 610 ;break;
+    case 16:
+        return 987 ;break;
+    case 17:
+        return 1597 ;break;
+    case 18:
+        return 2584 ;break;
+    case 19:
+        return 4181 ;break;
+    case 20:
+        return 6765 ;break;
+    case 21:
+        return 10946 ;break;
+    case 22:
+        return 17711 ;break;
+    case 23:
+        return 28657 ;break;
+    case 24:
+        return 46368 ;break;
+    case 25:
+        return 75025 ;break;
+    case 26:
+        return 121393 ;break;
+    case 27:
+        return 196418 ;break;
+    case 28:
+        return 317811 ;break;
+    case 29:
+        return 514229 ;break;
+    case 30:
+        return 832040 ;break;
+    default:
+        return -1;
+        break;
+    }
+}
